36a921669d2cf57126b36471a8dacfdaba994544
Wind <ao6swind@hotmail.com>
2018-09-19 23:16:58 +0800

    引入ckeditor

--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { IndexComponent } from './index/index.component';
-
+import { CKEditorModule } from 'ngx-ckeditor';
 @NgModule({
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    CKEditorModule
   ],
   declarations: [
     AboutComponent, 
